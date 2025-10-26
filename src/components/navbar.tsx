69b6b0fa5f5a9b201bfb04690b4a6b8e795ba57d
lucidmach <lucidmach@gmail.com>
2025-10-26 22:00:31 +1100

    feedback implementation on navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -23,7 +23,7 @@ const NavBar: React.FC = () => {
             loading="lazy"
             alt="the logo of unstitched"
           />
-          <h1 className="text-2xl">UnStitched</h1>
+          <h1 className="text-2xl">UnStitch</h1>
         </div>
       </div>
       <div>
@@ -31,36 +31,48 @@ const NavBar: React.FC = () => {
           className="p-3 border-t border-gray-300 flex flex-row items-center justify-between cursor-pointer"
           onClick={() => setOpen((state) => !state)}
         >
-          {/* <button onClick={(e) => setOpen(true)}> */}
-          <svg
-            clipRule="evenodd"
-            fillRule="evenodd"
-            strokeLinejoin="round"
-            strokeMiterlimit="2"
-            viewBox="0 0 24 24"
-            className={`w-10 h-10 ${open ? "invisible" : ""}`}
-            xmlns="http://www.w3.org/2000/svg"
+          {/* list svg */}
+          {open ? (
+            <></>
+          ) : (
+            <svg
+              clipRule="evenodd"
+              fillRule="evenodd"
+              strokeLinejoin="round"
+              strokeMiterlimit="2"
+              viewBox="0 0 24 24"
+              className="w-10 h-10"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="m13 16.745c0-.414-.336-.75-.75-.75h-9.5c-.414 0-.75.336-.75.75s.336.75.75.75h9.5c.414 0 .75-.336.75-.75zm9-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm-4-5c0-.414-.336-.75-.75-.75h-14.5c-.414 0-.75.336-.75.75s.336.75.75.75h14.5c.414 0 .75-.336.75-.75z"
+                fillRule="nonzero"
+              />
+            </svg>
+          )}
+          {/* close svg */}
+          {!open ? (
+            <></>
+          ) : (
+            <svg
+              clipRule="evenodd"
+              fillRule="evenodd"
+              strokeLinejoin="round"
+              strokeMiterlimit="2"
+              viewBox="0 0 24 24"
+              className="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" />
+            </svg>
+          )}
+          <h1
+            className={`text-xl flex-1  ${
+              open ? "invisible" : ""
+            } md:text-center text-right`}
           >
-            <path
-              d="m13 16.745c0-.414-.336-.75-.75-.75h-9.5c-.414 0-.75.336-.75.75s.336.75.75.75h9.5c.414 0 .75-.336.75-.75zm9-5c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75zm-4-5c0-.414-.336-.75-.75-.75h-14.5c-.414 0-.75.336-.75.75s.336.75.75.75h14.5c.414 0 .75-.336.75-.75z"
-              fillRule="nonzero"
-            />
-          </svg>
-          {/* </button> */}
-          <h1 className="text-xl">{sections[selected]}</h1>
-          {/* <button onClick={() => setOpen(false)}> */}
-          <svg
-            clipRule="evenodd"
-            fillRule="evenodd"
-            strokeLinejoin="round"
-            strokeMiterlimit="2"
-            viewBox="0 0 24 24"
-            className={`w-6 h-6 ${!open ? "invisible" : ""}`}
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" />
-          </svg>
-          {/* </button> */}
+            {sections[selected]}
+          </h1>
         </div>
         {open ? (
           <div className="flex flex-col p-3 space-y-3 border-t border-gray-300 items-center">
